47c69dd6650fb16dc52a1e4e7a0e97686b090e32
s2018w26 <s2018w26>
2020-09-17 09:45:03 +0900

    MEDIA_URL, MEDIA_ROOT(상품 그림 보이게)

--- shop/urls.py
+++ shop/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from shop import views
@@ -9,3 +11,5 @@ urlpatterns = [
     path('<category_slug>/', views.product_in_category, name='product_in_category'),
     path('<int:id>/<product_slug>/', views.product_detail, name='product_detail'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
